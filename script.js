a997afc8a728b626cc701a68c608c867862f4b91
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-05-15 11:29:03 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert("Hello world");
+// prompt("Enter your name: ");
+// console.log("This is a message");
+// console.warn("This is a warning message");
+// console.error("This is an error message");
+
+// Variables
+
+// var x; // Declaration of the variable
+// x=10; // Assignation of their value
+
+// let userAge = 99;
+// userAge = 98;
+// document.writeln(userAge);
+// var userName="Pancho";
+
+// document.writeln('<p>The users name is: ${userName}, and the age is ${userAge}</p>');
+
+// const taxes = 11;
+// document.writeln(taxes);
+
+let jobTitle="FullStack Developer";
+let geoLocation="San Diego";
+let numberChildren = 2;
+let partnerName="Katherine Schmidt";
+
+document.writeln('<p>You will be a ${jobTitle} in ${geoLocation}, married to ${partnerName} with ${numberChildren} children.');
