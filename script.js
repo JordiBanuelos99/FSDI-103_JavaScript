85d168bb332237a008836ac23f52352fca9ad77f
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-05-17 16:28:21 -0700

    Template string fix

--- script.js
+++ script.js
@@ -58,4 +58,25 @@ let video_Monetized=true;
 let video_YTshorts=false;
 let video_Visibility="public";
 
-document.writeln ('<p>Username: ' + userName + '</p><p>Channel: '+ ID_Youtubechannel + '</p><p>Subscribers: ' + ID_Subscribers + '</p><p>Join Date: ' + ID_joinDate + '</p><p>E-Mail: ' + ID_Email + '</p><p>Country: ' + ID_Location + '</p><p>Video Title: ' + video_Title + '</p><p>Original Filename: ' + video_OriginalFilename + '</p><p>Format: ' + video_Format + '</p><p>Description: ' + video_Description + '</p><p>Length (Hours): ' + video_LengthHours + ' hrs</p><p>Length (Minutes): ' + video_LengthMinutes + ' min</p><p>Length (Seconds): ' + video_LengthSeconds + 'sec</p><p>URL: ' + video_URL + '</p><p>Upload date: ' + video_UploadDate + '</p><p>Views: ' + video_Views + '</p><p>Likes: ' + video_Likes + '</p><p>Comments: ' + video_Comments + '</p><p>Monetization: ' + video_Monetized + '</p><p>Youtube shorts: ' + video_YTshorts + '</p><p>Visibility: ' + video_Visibility);
+document.writeln (
+    `<p>Username: ${userName} </p>
+    <p>Channel: ${ID_Youtubechannel}</p>
+    <p>Subscribers: ${ID_Subscribers}</p>
+    <p>Join Date: ${ID_joinDate} </p>
+    <p>E-Mail: ${ID_Email}</p>
+    <p>Country: ${ID_Location}</p>
+    <p>Video Title: ${video_Title}</p>
+    <p>Original Filename: ${video_OriginalFilename}</p>
+    <p>Format: ${video_Format}</p>
+    <p>Description: ${video_Description}</p>
+    <p>Length (Hours): ${video_LengthHours} hrs</p>
+    <p>Length (Minutes): ${video_LengthMinutes} min</p>
+    <p>Length (Seconds): ${video_LengthSeconds} sec</p>
+    <p>URL: ${video_URL}</p>
+    <p>Upload date: ${video_UploadDate}</p>
+    <p>Views: ${video_Views}</p>
+    <p>Likes: ${video_Likes}</p>
+    <p>Comments: ${video_Comments}</p>
+    <p>Monetization: ${video_Monetized}</p>
+    <p>Youtube shorts: ${video_YTshorts}</p>
+    <p>Visibility: ${video_Visibility}</p>`);
