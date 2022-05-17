dda56bdf5042bf1fcf919b92cbb8432c5c1da01c
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-05-17 15:59:33 -0700

    First Assignment

--- script.js
+++ script.js
@@ -19,9 +19,43 @@
 // const taxes = 11;
 // document.writeln(taxes);
 
-let jobTitle="FullStack Developer";
-let geoLocation="San Diego";
-let numberChildren = 2;
-let partnerName="Katherine Schmidt";
+// let jobTitle="FullStack Developer";
+// let geoLocation="San Diego";
+// let numberChildren = 2;
+// let partnerName="Katherine Schmidt";
 
-document.writeln('<p>You will be a ${jobTitle} in ${geoLocation}, married to ${partnerName} with ${numberChildren} children.');
+// document.writeln('<p>You will be a ${jobTitle} in ${geoLocation}, married to ${partnerName} with ${numberChildren} children.');
+
+/* let workerName = "John Smith";
+let E_Mail = "JohnSmith@mail.com";
+let Password ="QWERTY567";
+let age = 34;
+let country = "United States";
+let monthly_salary = 3000;
+let year_Salary = (monthly_salary*12);
+
+document.write(' <p> Name: + ${workerName} + </p> <p> EMail: ${E_Mail}</p> <p> Annual Salary: ${year_Salary}</p>'); */
+
+let userName="Clipset";
+let ID_Youtubechannel="https://www.youtube.com/c/clipset";
+let ID_Subscribers=2430000;
+let ID_joinDate='2006-12-22';
+let ID_Email="clipset@clipset.net";
+let ID_Location="Spain";
+let video_Title="iPhone6 Review";
+let video_OriginalFilename="iPhone6_-_Review.mp4";
+let video_Format="mp4";
+let video_Description='Probamos el iPhone 6, uno de los últimos lanzamientos de Apple, que apuesta por una mayor pantalla de 4,7", pero por debajo de la del iPhone 6 Plus, del que os hablaremos también en muy pocos días.';
+let video_LengthHours=0;
+let video_LengthMinutes=13;
+let video_LengthSeconds=32;
+let video_URL="https://youtu.be/Iffyu3C-wEw";
+let video_UploadDate='2014-09-24';
+let video_Views=3015359;
+let video_Likes=27604;
+let video_Comments=3179;
+let video_Monetized=true;
+let video_YTshorts=false;
+let video_Visibility="public";
+
+document.writeln ('<p>Username: ' + userName + '</p><p>Channel: '+ ID_Youtubechannel + '</p><p>Subscribers: ' + ID_Subscribers + '</p><p>Join Date: ' + ID_joinDate + '</p><p>E-Mail: ' + ID_Email + '</p><p>Country: ' + ID_Location + '</p><p>Video Title: ' + video_Title + '</p><p>Original Filename: ' + video_OriginalFilename + '</p><p>Format: ' + video_Format + '</p><p>Description: ' + video_Description + '</p><p>Length (Hours): ' + video_LengthHours + ' hrs</p><p>Length (Minutes): ' + video_LengthMinutes + ' min</p><p>Length (Seconds): ' + video_LengthSeconds + 'sec</p><p>URL: ' + video_URL + '</p><p>Upload date: ' + video_UploadDate + '</p><p>Views: ' + video_Views + '</p><p>Likes: ' + video_Likes + '</p><p>Comments: ' + video_Comments + '</p><p>Monetization: ' + video_Monetized + '</p><p>Youtube shorts: ' + video_YTshorts + '</p><p>Visibility: ' + video_Visibility);
